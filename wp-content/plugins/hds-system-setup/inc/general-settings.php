282ce934a46893c25e0e55734e39beaac4c9023c
azuborev <azuborev@corp.web4pro.com.ua>
2021-04-21 17:53:29 +0300

    add plugin random id

--- wp-content/plugins/hds-system-setup/inc/general-settings.php
+++ wp-content/plugins/hds-system-setup/inc/general-settings.php
@@ -5,3 +5,31 @@
  * ------------------------------
  */
 
+if ( function_exists( 'acf_add_options_page' ) ) {
+
+	acf_add_options_page(
+		array(
+			'page_title' => __( 'Theme General Settings', 'w4ptheme' ),
+			'menu_title' => __( 'Theme Settings', 'w4ptheme' ),
+			'menu_slug'  => 'theme-general-settings',
+			'capability' => 'edit_posts',
+			'redirect'   => false,
+		)
+	);
+
+	acf_add_options_sub_page(
+		array(
+			'page_title'  => __( 'Theme Header Settings', 'w4ptheme' ),
+			'menu_title'  => __( 'Header', 'w4ptheme' ),
+			'parent_slug' => 'theme-general-settings',
+		)
+	);
+
+	acf_add_options_sub_page(
+		array(
+			'page_title'  => __( 'Theme Footer Settings', 'w4ptheme' ),
+			'menu_title'  => __( 'Footer', 'w4ptheme' ),
+			'parent_slug' => 'theme-general-settings',
+		)
+	);
+}
