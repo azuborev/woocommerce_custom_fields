1e436010375a2d418ccfc4d414a135a56975ef51
azuborev <azuborev@corp.web4pro.com.ua>
2021-04-21 16:51:09 +0300

    add plugins

---
+++ wp-content/plugins/hds-system-setup/hds-system-setup.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * Plugin Name: HDS SYSTEM SETUP
+ * Description: Plugin for adding HDS settings
+ * Author: W4P AZuborev
+ * Version: 1.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+require_once( __DIR__ . '/inc/general-settings.php' );
+
+
