e389f575b8464a03d1547250d166a8f60b1d3004
azuborev <zuborev.kh@gmail.com>
2021-04-26 02:33:37 +0300

    wpdb insert func

--- wp-content/plugins/hds-system-setup/hds-system-setup.php
+++ wp-content/plugins/hds-system-setup/hds-system-setup.php
@@ -27,8 +27,8 @@ function create_table_users_relationship()
     $charset_collate = "DEFAULT CHARACTER SET {$wpdb->charset} COLLATE {$wpdb->collate}";
 
     $sql = "CREATE TABLE {$table_name} (
+    user_id  bigint(20) unsigned NOT NULL,
 	private_id  bigint(20) unsigned NOT NULL auto_increment,
-	user_id  bigint(20) unsigned NOT NULL,
 	PRIMARY KEY  (private_id),
 	FOREIGN KEY  (user_id) REFERENCES {$users_table}(id)
     )
@@ -40,7 +40,51 @@ function create_table_users_relationship()
 
 register_activation_hook( __FILE__, 'create_table_users_relationship' );
 
+/**
+ * Add 'private_id' for registered user with role - doctor, clinic or med_pred
+ * @param $user_id
+ */
+function hds_add_private_id1 ( $user_id ) {
+    if ( user_can( $user_id, 'hds_doctor' ) ||
+        user_can( $user_id, 'hds_clinic' ) ||
+        user_can( $user_id, 'hds_med_pred' )
+    ) {
 
+
+        global $wpdb;
+        $table = $wpdb->prefix.'hds_user_relationship';
+        $data = array('user_id' => $user_id);
+        $format = array('%d');
+        $wpdb->insert($table,$data,$format);
+
+
+    } else {
+        delete_user_meta( $user_id, 'private_id' );
+    }
+}
+add_action( 'user_register', 'hds_add_private_id1' );
+
+
+
+/**
+ * Render private_id on user profile pages
+ * @param $user
+ */
+function hds_show_user_private_id( $user ) {
+    ?>
+    <?php $private_id = get_the_author_meta( 'medical_id', $user->ID ); ?>
+        <?php if ( ! empty( $private_id ) || in_array( 'customer', $user->roles ) ) : ?>
+    <table class="form-table medical-table">
+        <tr>
+            <th><label for="medical_id"><?php esc_html_e( 'ID клиники / врача 1', 'hds-settings' ); ?></label></th>
+            <td><?php echo esc_attr( $private_id ); ?></td>
+        </tr>
+    </table>
+        <?php endif; ?>
+    <?php
+}
+add_action( 'show_user_profile', 'hds_show_user_private_id' );
+add_action( 'edit_user_profile', 'hds_show_user_private_id' );
 
 
 
